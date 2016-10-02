92284a4c697ad3533df6d0064820d3a1fed777f1
david.gao <r0011@126.com>
2016-10-02 13:30:10 +0800

    init project

---
+++ src/main/java/ort/study/akka/helloworld/AkkaHelloWorld.java
@@ -0,0 +1,15 @@
+package ort.study.akka.helloworld;
+
+/**
+ * .
+ * User: gaojiawei
+ * Date: 16/10/2
+ * qunar.com
+ */
+public class AkkaHelloWorld {
+    public static void main(String[] args) {
+        String test = "helloWorld";
+        System.out.println(test);
+    }
+
+}
